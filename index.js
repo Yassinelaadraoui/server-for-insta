ee28ffa81d1ce6d55c707f6205f3cf6a1d913842
yassinelaadraoui <yassinelaadraoui@gmail.com>
2017-02-27 14:23:41 +0200

    more data

--- index.js
+++ index.js
@@ -127,6 +127,9 @@ var weatherData = [
 var searchedpost = [
 
 	]
+var searchedtag = [
+
+	]
 app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
